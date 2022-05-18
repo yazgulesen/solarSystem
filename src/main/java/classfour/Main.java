643687728ab7da11a4de008013168139c7ce81b7
yazgulesen <iamyazgul@gmail.com>
2022-05-17 20:12:19 -0400

    feature star added but with error

--- src/main/java/classfour/Main.java
+++ src/main/java/classfour/Main.java
@@ -7,11 +7,19 @@ public class Main {
         factorX.Moon="4";
         factorX.Planet="2";
         factorX.Stars="10000";
-//adding features to our son
+//adding features to our sun
         FeatureSun Xfact = new FeatureSun();
         Xfact.color="green";
         Xfact.radius="1000000000km";
         Xfact.heat="309324934898kj";
+
+        //adding the features for star in the solar system
+        FeatureStarts facX = new FeatureStarts();
+        FeatureStarts FacX = new FeatureStarts();
+        FacX.color=green;
+        FacX.radius="738y4932749km";
+        FacX.size="9999999999m";
+
     }
 
 }
